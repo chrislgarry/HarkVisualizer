929596a913c0076361f7e7d10070dc1ef275b836
Chris Garry <chrislgarry@gmail.com>
2016-06-30 22:06:33 +0000

    Heavy refactoring of main module. Added loading.

--- harkvisualizer.py
+++ harkvisualizer.py
@@ -1,3 +1,4 @@
+import atexit
 from datetime import timedelta 
 import json 
 import logging as log 
@@ -10,8 +11,8 @@ import speech_recognition
 from tornado import web, ioloop, websocket 
 from werkzeug.utils import secure_filename 
  
-UPLOAD_PATH = '/tmp/' 
-ALLOWED_EXTENSIONS = set(['flac', 'wav', 'ogg']) 
+STAGING_AREA = '/tmp/' 
+ALLOWED_EXTENSIONS = set(['flac', 'wav']) 
 STATIC_PATH = 'static' 
 HTML_TEMPLATE_PATH = 'templates' 
 LISTEN_PORT = 80 
@@ -27,7 +28,7 @@ settings = {
 'template_path': path.join(path.dirname(__file__), HTML_TEMPLATE_PATH) 
 } 
  
-metadata = { 
+default_hark_config = { 
     'processType': 'batch', 
     'params': { 
         'numSounds': 2, 
@@ -54,109 +55,143 @@ class HttpRequestHandler(web.RequestHandler):
         file = self.request.files['file'][0] 
         if file and allowed_file(file['filename']): 
             file_name = secure_filename(file['filename']) 
-            audio_file = UPLOAD_PATH + file_name 
+            audio_file = STAGING_AREA + file_name 
             write_handle = open(audio_file, 'w') 
             write_handle.write(file['body']) 
-            read_handle = open(audio_file, 'rb') 
-            hark_client.createSession(metadata) 
-            hark_client.uploadFile(read_handle) 
-            self.render('visualize.html') 
+            read_handle = open(audio_file, 'rb')
+            hark.client.createSession(default_hark_config)
+            hark.upload_file(read_handle) 
+            self.render('visualize.html')
  
- 
-class HarkSaas: 
- 
- 
-    def client(self): 
+# Wrapper around PyHarkSaas 
+class Hark: 
+
+
+    def __init__(self):
+        log.info('Initializing hark client') 
         auth = json.load(open('harkauth.json')) 
         client = pyhark.saas.PyHarkSaaS(auth['apikey'], auth['apisec']) 
         client.login() 
-        return client 
- 
- 
+        self.client = client
+  
+    def get_audio(self, srcID, file_name):
+        log.info('Retrieving separated audio %d from hark', srcID) 
+        with open(file_name, 'w') as write_handle:
+            self.client.getSeparatedAudio(handle=write_handle,
+                                          srcID=srcID)
+
+    def delete_session(self):
+        log.info('Deleting hark session %s', self.client.getSessionID()) 
+        self.client.deleteSession()
+
+    def upload_file(self, file_handle):
+        log.info('Uploading file %s to hark', file_handle.name) 
+        self.client.uploadFile(file_handle)
+
+    def get_results(self):
+        return self.client.getResults()
+
+    def wait(self):
+        self.client.wait()
+
+
+# Wrapper around SpeechRecognition
+class Speech:
+
+    
+    def __init__(self):
+        log.info('Initializing speech client')
+        self.auth = str(json.load(open('bingauth.json'))['apikey'])
+        self.client = speech_recognition.Recognizer()
+
+    def translate(self, file_name):
+        transcription = 'Inaudible'
+        with speech_recognition.AudioFile(path.join(path.dirname(
+            path.realpath(__file__)), file_name)) as source:
+            audio = self.client.record(source)
+        log.info('Sending separated audio file to speech api')
+        try:
+            transcription = self.client.recognize_bing(audio,
+                key=self.auth,
+                language=LANGUAGE) 
+        # This exception should pass if there is no transcription. 
+        except speech_recognition.UnknownValueError: 
+            pass  
+        log.info('Transcription: %s', transcription) 
+        return transcription
+
+
 class WebSocketHandler(websocket.WebSocketHandler): 
  
  
-    #allow cross-origin web socket connection 
+    # Allow cross-origin web socket connections 
     def check_origin(self, origin): 
         return True 
 
-    def clean_staging(self):
-        uploads = listdir(UPLOAD_PATH)
-        for file in uploads:
-            remove(UPLOAD_PATH + file)
-        log.info('Deleted contents of upload path')
-        hark_client.deleteSession()
-        log.info('Deleted hark session')
-
-    # Invoked when anyone closes the socket
+    # Invoked when socket closes or program exits
     def on_connection_close(self):
-        self.clean_staging()
+        log.info('Web socket connection closed')
+        clean_staging()
 
-    # Called upon open of this socket 
+    # Invoked when socket is opened 
     def open(self): 
-        log.info('Connection established') 
+        log.info('Web socket connection established') 
         # ioloop to wait for 2 seconds before sending data 
         ioloop.IOLoop.instance().add_timeout(timedelta(seconds=2),  
                                              self.send_data) 
  
     def send_data(self): 
-        speech_client = speech_recognition.Recognizer(); 
-        auth = json.load(open('bingauth.json'))
-        log.info('Hark is processing the file') 
-        hark_client.wait() 
-        data = hark_client.getResults() 
-        log.info('Hark processing complete') 
+        log.info('Hark is analyzing the file') 
+        hark.wait() 
+        data = hark.get_results() 
+        log.info('Hark analysis results:') 
         log.info(str(data['context'])) 
         if data['context']: 
             for entry in data['context']: 
- 
-                utterance = 'Inaudible' 
-                file_name = UPLOAD_PATH + 'part' + str(entry['srcID']) + '.flac' 
+                srcID = entry['srcID'] 
+                file_name = '{0}part{1}.flac'.format(STAGING_AREA, srcID)
                 log.info('Creating separated audio file handle %s', file_name) 
-                with open(file_name, 'w') as write_handle: 
-                    hark_client.getSeparatedAudio(handle=write_handle,  
-                                                 srcID=entry['srcID']) 
-                log.info('Retrieved and saved separated audio %d from hark', entry['srcID']) 
-                with speech_recognition.AudioFile(path.join(path.dirname( 
-                    path.realpath(__file__)), file_name)) as source: 
-                    audio = speech_client.record(source) 
-                log.info('Sending separated audio file to speech api') 
- 
-                try: 
-                    utterance = speech_client.recognize_bing(audio,  
-                        key=str(auth['apikey']),  
-                        language=LANGUAGE) 
-                    log.info('Transcription for part %d: %s', entry['srcID'], utterance) 
-                # Thrown if utterance cannot be translated to text. 
-                # It is acceptable to pass this exception and proceed. 
-                except speech_recognition.UnknownValueError: 
-                    log.info('Transcription unavailable for %d', entry['srcID']) 
-                    pass 
+                hark.get_audio(srcID, file_name)
+                transcription = speech.translate(file_name)
+                log.info('Removing separated audio file %s', file_name)
                 remove(file_name) 
-                log.info('Removed separated audio file %s', file_name)
-                self.write_message(utterance) 
-                #create new ioloop instance to intermittently publish data 
-                #ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1), self.send_data) 
+                log.info('Writing transcription to socket')
+                self.write_message(transcription) 
 
 
 # Helper functions 
+
+
 def allowed_file(file_name): 
     extension = file_name.rsplit('.', 1)[1] 
     return '.' in file_name and extension in ALLOWED_EXTENSIONS 
- 
-if __name__ == '__main__': 
- 
- 
-    log.info('Initializing third-party clients') 
-    hark_saas = HarkSaas() 
-    hark_client = hark_saas.client() 
-    log.info('Initializing web application') 
+
+def clean_staging():
+   remove_all(STAGING_AREA)
+   hark.delete_session()
+
+def get_app():
     application = web.Application([ 
         (r'/', HttpRequestHandler), 
         (r'/websocket', WebSocketHandler), 
         (r'/(apple-touch-icon\.png)', web.StaticFileHandler, 
             dict(path=settings['static_path'])), 
         ], **settings) 
-    application.listen(LISTEN_PORT) 
+    return application
+
+def remove_all(dir):
+    log.info('Deleting contents of %s', dir)
+    files = listdir(dir)
+    for file in files:
+        remove(dir + file)
+ 
+ 
+if __name__ == '__main__': 
+    hark = Hark()
+    speech = Speech() 
+    atexit.register(clean_staging)
+    log.info('Initializing web application') 
+    app = get_app()
+    app.listen(LISTEN_PORT) 
     ioloop.IOLoop.instance().start() 
 
