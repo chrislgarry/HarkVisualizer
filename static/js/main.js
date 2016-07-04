5b48a18fbcc9cbec6dc491363cd8cdb7be7c0c22
chrislgarry <chrislgarry@gmail.com>
2016-07-04 18:55:08 -0400

    Add javascript in separate js file

---
+++ static/js/main.js
@@ -0,0 +1,71 @@
+// Set the chatbox to scroll overflow when text exceeds element size
+document.getElementById("chatbox").style.overflow = "scroll";
+
+var connection = new WebSocket('ws://harkvisualizer.com/websocket');
+
+var durationRingChart = dc.pieChart("#chart-ring-duration"),
+    speakersRowChart = dc.rowChart("#chart-row-speakers");
+
+var xfilter = crossfilter(),
+    durationDim = xfilter.dimension(function(d) {return Math.floor(+d.duration);}),
+    azimuthDim = xfilter.dimension(function(d) {return Math.floor(+d.azimuth);}),
+    guidDim  = xfilter.dimension(function(d) {return d.guid;}),
+  
+    durationPerAzim = azimuthDim.group().reduceSum(function(d) {return +d.duration;}),
+    durationPerName = guidDim.group().reduceSum(function(d) {return +d.duration;});
+
+function render_plots(){
+    durationRingChart
+        .width(250).height(250)
+        .dimension(azimuthDim)
+        .group(durationPerAzim)
+        .innerRadius(50)
+        .colors(d3.scale.category10())
+        .label(function(d) { return d.key + "\xB0"; })
+        .renderTitle(true);
+    speakersRowChart
+        .width(400).height(250)
+        .dimension(guidDim)
+        .group(durationPerName)
+        .elasticX(true)
+        .colors(d3.scale.category10())
+    dc.renderAll();
+}
+
+render_plots();
+
+// Resets the graphs
+function resetData() {
+    var durationChartFilters = durationRingChart.filters();
+    var speakerChartFilters = speakersRowChart.filters();
+    durationRingChart.filter(null);
+    speakersRowChart.filter(null);
+    xfilter.remove();
+    durationRingChart.filter([durationChartFilters]);
+    speakersRowChart.filter([speakerChartFilters]);
+}
+
+// When a socket message is received
+connection.onmessage = function(event) {
+    console.log(event.data)
+    var message = JSON.parse(event.data);
+    // If utterance is a context result
+    if(message.guid){
+        // Extract values and render in graph
+        guid = message.guid
+        azimuth = message.azimuth
+        duration = message.duration
+        var updateObject =[{
+            "guid": guid,
+            "azimuth": azimuth,
+           "duration": duration
+        }]
+        xfilter.add(updateObject);
+        dc.redrawAll();
+    }
+    // Message is a transcription 
+    else{
+        // Render transcription in chat box
+        document.getElementById('chatbox').innerHTML += "<p id=\"message\">" + message + "</p>";
+    }
+}
